a7e4b136712385400e95354206c69f1352eb680c
Brandon Robare <brandon.robare@gmail.com>
2022-04-10 11:52:17 -0700

    Updates for APC

--- fakenos/plugins/nos/__init__.py
+++ fakenos/plugins/nos/__init__.py
@@ -1,9 +1,9 @@
 from . import cisco_ios
 from . import accedian
-from . import apc_nmc
+from . import apc
 
 nos_plugins = {
     "cisco_ios": cisco_ios,
     "accedian": accedian,
-    "apc": apc_nmc,
+    "apc": apc,
 }
